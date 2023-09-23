e02aa43433669e15bb251f430c36333e488f3815
maksymdidukh <maxym.didukh@logivations.com>
2023-09-23 23:03:44 +0200

    make some pure virtual methods non-virtual

--- GraphImplementation.cpp
+++ GraphImplementation.cpp
@@ -1,6 +1,6 @@
 #include "GraphImplementation.hpp"
 
-Graph::Graph(unsigned int _numVertices, bool _directed) : numVertices(_numVertices), directed(_directed) {
+Graph::Graph(unsigned int _numVertices) : numVertices(_numVertices) {
     adjMatrix.reserve(numVertices + int(numVertices * 0.2));
     adjMatrix = std::vector<std::vector<int>>(numVertices, std::vector<int>(numVertices, 0));
 
@@ -8,14 +8,44 @@ Graph::Graph(unsigned int _numVertices, bool _directed) : numVertices(_numVertic
         row.reserve(numVertices + int(numVertices * 0.2));
 }
 
-Graph::~Graph() {}
-
 int Graph::getNumVertices() const {
     return numVertices;
 }
 
 std::vector<std::vector<int>> Graph::getAdjMatrix() const {
     return adjMatrix;
+}
+
+void Graph::removeEdge(int from, int to) {
+    if (from >= 0 && from < numVertices && to >= 0 && to < numVertices) {
+        adjMatrix.at(from).at(to) = adjMatrix.at(to).at(from) = 0;
+    }
+}
+
+bool Graph::containsEdge(int from, int to) const {
+    if (from >= 0 && from < numVertices && to >= 0 && to < numVertices) {
+        return adjMatrix.at(from).at(to) != 0;
+    }
+    return false;
+}
+
+void Graph::removeVertex(int vertex) {
+    if(vertex >= 0 && vertex <= numVertices) {
+        adjMatrix.erase(adjMatrix.begin() + vertex);
+        for(auto& row : adjMatrix)
+            row.erase(row.begin() + vertex);
+    }
+
+    numVertices--;
+} 
+
+void Graph::addVertex() {
+    adjMatrix.push_back(std::vector<int>(numVertices, 0));
+    numVertices++;
+
+    for(auto& row : adjMatrix) {
+        row.push_back(0);
+    }
 }
 
 void Graph::printAdjMatrix() const {
@@ -31,7 +61,7 @@ float Graph::calculateProbability(const float constant) {
     return (std::log(numVertices)  * constant) / numVertices;
 }
 
-UndirectedGraph::UndirectedGraph(unsigned int _numVertices) : Graph(_numVertices, false) {}
+UndirectedGraph::UndirectedGraph(unsigned int _numVertices) : Graph(_numVertices) {}
 
 bool UndirectedGraph::isDirected() const {
     return false;
@@ -45,39 +75,6 @@ void UndirectedGraph::addEdge(int from, int to) {
         adjMatrix.at(from).at(to) = adjMatrix.at(to).at(from) = 1;
     }
 }
-
-void UndirectedGraph::removeEdge(int from, int to) {
-    if (from >= 0 && from < numVertices && to >= 0 && to < numVertices) {
-        adjMatrix.at(from).at(to) = adjMatrix.at(to).at(from) = 0;
-    }
-}
-
-bool UndirectedGraph::containsEdge(int from, int to) const {
-    if (from >= 0 && from < numVertices && to >= 0 && to < numVertices) {
-        return adjMatrix.at(from).at(to) == 1;
-    }
-    return false;
-}
-
-void UndirectedGraph::addVertex() {
-    adjMatrix.push_back(std::vector<int>(numVertices, 0));
-    numVertices++;
-
-    for(auto& row : adjMatrix) {
-        row.push_back(0);
-    }
-}
-
-void UndirectedGraph::removeVertex(int vertex) {
-    if(vertex >= 0 && vertex <= numVertices) {
-        adjMatrix.erase(adjMatrix.begin() + vertex);
-        for(auto& row : adjMatrix)
-            row.erase(row.begin() + vertex);
-    }
-
-    numVertices--;
-} 
-
 
 void UndirectedGraph::ErdosRenyiModelGeneration(const float constant) {
     std::random_device randomDevice;
@@ -93,6 +90,22 @@ void UndirectedGraph::ErdosRenyiModelGeneration(const float constant) {
             else 
                 adjMatrix.at(i).at(j) = adjMatrix.at(j).at(i) = 0;
         }
+    }
+}
+
+DirectedGraph::DirectedGraph(unsigned int _numVertices) : Graph::Graph(_numVertices) {}
+
+bool DirectedGraph::isDirected() const {
+    return true;
+}
+
+void DirectedGraph::addEdge(int from, int to) {
+    if (from == to)
+        return;
+
+    if (from >= 0 && from < numVertices && to >= 0 && to < numVertices) {
+        adjMatrix.at(from).at(to) = 1;
+        adjMatrix.at(to).at(from) = -1;
     }
 }
 
--- GraphImplementation.hpp
+++ GraphImplementation.hpp
@@ -8,23 +8,22 @@
 class Graph {
 protected:
     unsigned int numVertices;
-    bool directed;
     std::vector<std::vector<int>> adjMatrix;
 
 public:
-    Graph(unsigned int _numVertices, bool _directed);
-    ~Graph();
+    Graph(unsigned int _numVertices);
+    ~Graph() = default;
 
     virtual bool isDirected() const = 0;
     int getNumVertices() const;
     std::vector<std::vector<int>> getAdjMatrix() const;
 
     virtual void addEdge(const int from, const int to) = 0;
-    virtual void removeEdge(const int from, const int to) = 0;
-    virtual bool containsEdge(const int from, const int to) const = 0;
+    void removeEdge(const int from, const int to);
+    bool containsEdge(const int from, const int to) const;
 
-    virtual void addVertex() = 0;
-    virtual void removeVertex(const int vertex) = 0;
+    void addVertex();
+    void removeVertex(const int vertex);
 
     float calculateProbability(const float constant);
     virtual void ErdosRenyiModelGeneration(const float constant) = 0;
@@ -33,19 +32,19 @@ public:
 };
 
 class UndirectedGraph : public Graph {
-private:
-
 public:
     UndirectedGraph(unsigned int _numVertices);
 
     bool isDirected() const override;
-
     void addEdge(const int from, const int to) override;
-    void removeEdge(const int from, const int to) override;
-    bool containsEdge(const int from, const int to) const override;
-
-    void addVertex() override;
-    void removeVertex(const int vertex) override;
-
     void ErdosRenyiModelGeneration(const float constant) override;
 };
+
+class DirectedGraph : public Graph {
+public: 
+    DirectedGraph(unsigned int _numVertices);
+
+    bool isDirected() const override;
+    void addEdge(const int from, const int to) override;
+    void ErdosRenyiModelGeneration(const float constant) override;
+} ;
